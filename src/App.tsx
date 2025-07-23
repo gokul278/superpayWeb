6e1a99e2c46c1b6ca0ebda577e9c4edb7c529c62
GOKUL M <82570950+gokul278@users.noreply.github.com>
2025-07-23 23:34:24 +0530

    Fix content display issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
+import AboutSection from './components/AboutSection';
 import BusinessOverview from './components/BusinessOverview';
 import TechnicalRequirements from './components/TechnicalRequirements';
 import FinancialProjections from './components/FinancialProjections';
 import ComplianceSection from './components/ComplianceSection';
+import PoliciesSection from './components/PoliciesSection';
 import PoliciesSection from './components/PoliciesSection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
@@ -16,10 +18,12 @@ function App() {
       <Header />
       <HeroSection />
       <AboutSection />
+      <AboutSection />
       <BusinessOverview />
       <TechnicalRequirements />
       <FinancialProjections />
       <ComplianceSection />
+      <PoliciesSection />
       <PoliciesSection />
       <ContactSection />
       <Footer />
